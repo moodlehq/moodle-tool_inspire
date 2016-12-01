cfe51413b151ccc7d64d3b52b358eb6c631fd685
David Monllao <davidm@moodle.com>
2016-12-01 12:09:10 +0800

    MDL-57791 inspire: Adding the ml processors subplugin system

--- lang/en/tool_research.php
+++ lang/en/tool_research.php
@@ -22,12 +22,17 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['errormlwrongformat'] = 'The machine learning processor return can not be decoded: "{$a}"';
+$string['errornomlresults'] = 'No results returned from the machine learning processor, check the output directory contents for more info';
 $string['errornorunrecord'] = 'Couldn\'t locate the current run record in the database';
 $string['errornoroles'] = 'Student or teacher roles have not been defined';
+$string['modeloutputdir'] = 'Models output directory';
 $string['nocourses'] = 'No courses to analyse';
-$string['skippingcourse'] = 'Skipping course {$a}';
 $string['processingcourse'] = 'Processing course {$a}';
 $string['processingcourses'] = 'Processing courses...';
 $string['pluginname'] = 'Research';
+$string['skippingcourse'] = 'Skipping course {$a}';
 $string['studentroles'] = 'Student roles';
+$string['subplugintype_ml'] = 'Machine learning processor';
+$string['subplugintype_ml_plural'] = 'Machine learning processors';
 $string['teacherroles'] = 'Teacher roles';
