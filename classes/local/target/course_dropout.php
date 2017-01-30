f6522f12a9a2f19947bb9e2e23c2521edfb010ee
David Monllao <david.monllao@gmail.com>
2017-01-30 16:24:23 +0100

    MDL-57791 inspire: Course dropout refinements

--- classes/local/target/course_dropout.php
+++ classes/local/target/course_dropout.php
@@ -66,31 +66,39 @@ class course_dropout extends binary {
         return '\\tool_inspire\\local\\analyser\\enrolments';
     }
 
-    public function is_valid_analysable(\tool_inspire\analysable $analysable) {
+    public function is_valid_analysable(\tool_inspire\analysable $course) {
         global $DB;
 
+        if (!$course->was_started()) {
+            return 'Course is not yet started';
+        }
+
         // Ongoing courses data can not be used to train.
-        if ($analysable->get_end() > time()) {
+        if (!$course->is_finished()) {
             return 'Course is not yet finished';
         }
 
         // Courses that last more than 1 year may not have a regular usage.
-        if ($analysable->get_end() - $analysable->get_start() > YEARSECS) {
+        if ($course->get_end() - $course->get_start() > YEARSECS) {
             return 'Duration is more than 1 year';
+        }
+
+        if (!$students = $course->get_students()) {
+            return 'No students';
         }
 
         // Not a valid target if there are not enough course accesses.
         // Using anonymous to use the db index, not filtering by timecreated to speed it up.
-        $params = array('courseid' => $analysable->get_id(), 'anonymous' => 0, 'start' => $analysable->get_start(),
-            'end' => $analysable->get_end());
-        list($studentssql, $studentparams) = $DB->get_in_or_equal(array_keys($analysable->get_students()), SQL_PARAMS_NAMED);
+        $params = array('courseid' => $course->get_id(), 'anonymous' => 0, 'start' => $course->get_start(),
+            'end' => $course->get_end());
+        list($studentssql, $studentparams) = $DB->get_in_or_equal($students, SQL_PARAMS_NAMED);
         $select = 'courseid = :courseid AND anonymous = :anonymous AND timecreated > :start AND timecreated < :end ' .
             'AND userid ' . $studentssql;
         $nlogs = $DB->count_records_select('logstore_standard_log', $select, array_merge($params, $studentparams));
 
         // Say 5 logs per week by half of the course students.
-        $nweeks = $this->get_time_range_weeks_number($analysable->get_start(), $analysable->get_end());
-        $nstudents = count($analysable->get_students());
+        $nweeks = $this->get_time_range_weeks_number($course->get_start(), $course->get_end());
+        $nstudents = count($course->get_students());
         if ($nlogs < ($nweeks * ($nstudents / 2) * 5)) {
             return 'Not enough logs';
         }
@@ -101,22 +109,22 @@ class course_dropout extends binary {
         $nocompletion = false;
         $nocompetencies = false;
 
-        $completion = new \completion_info($analysable->get_id());
+        $completion = new \completion_info($course->get_course_data());
         if (!$completion->is_enabled() && $completion->has_criteria()) {
             $nocompletion = true;
         }
 
-        if (\core_competency\api::count_competencies_in_course($analysable->get_id()) === 0) {
+        if (\core_competency\api::is_enabled() && \core_competency\api::count_competencies_in_course($course->get_id()) === 0) {
             $nocompetencies = true;
         }
 
         // Not a valid target if there is no course grade item.
-        self::$coursegradeitems[$analysable->get_id()] = \grade_item::fetch(array('itemtype' => 'course', 'courseid' => $analysable->get_id()));
-        if (empty(self::$coursegradeitems[$analysable->get_id()])) {
+        self::$coursegradeitems[$course->get_id()] = \grade_item::fetch(array('itemtype' => 'course', 'courseid' => $course->get_id()));
+        if (empty(self::$coursegradeitems[$course->get_id()])) {
             $nogradeitem = true;
         }
 
-        if (self::$coursegradeitems[$analysable->get_id()]->grade_type !== GRADE_TYPE_VALUE) {
+        if (self::$coursegradeitems[$course->get_id()]->gradetype !== GRADE_TYPE_VALUE) {
             $nogradevalue = true;
         }
 
@@ -138,16 +146,16 @@ class course_dropout extends binary {
      *
      * @param int $sampleid
      * @param string $tablename
-     * @param \tool_inspire\analysable $analysable
+     * @param \tool_inspire\analysable $course
      * @param array $data
      * @return void
      */
-    public function calculate_sample($sampleid, $tablename, \tool_inspire\analysable $analysable, $data) {
+    public function calculate_sample($sampleid, $tablename, \tool_inspire\analysable $course, $data) {
 
         // We use completion as a success metric only when it is enabled.
-        $completion = new \completion_info($analysable->get_id());
+        $completion = new \completion_info($course->get_course_data());
         if ($completion->is_enabled() && $completion->has_criteria()) {
-            $ccompletion = new \completion_completion(array('userid' => $sampleid, 'course' => $analysable->get_id()));
+            $ccompletion = new \completion_completion(array('userid' => $sampleid, 'course' => $course->get_id()));
             if ($ccompletion->is_complete()) {
                 return 0;
             } else {
@@ -156,14 +164,16 @@ class course_dropout extends binary {
         }
 
         // Same with competencies.
-        $ncoursecompetencies = \core_competency\api::count_competencies_in_course($analysable->get_id());
-        if ($ncoursecompetencies > 0) {
-            $nusercompetencies = \core_competency\api::count_proficient_competencies_in_course_for_user(
-                $analysable->get_id(), $sampleid);
-            if ($ncoursecompetencies > $nusercompetencies) {
-                return 1;
-            } else {
-                return 0;
+        if (\core_competency\api::is_enabled()) {
+            $ncoursecompetencies = \core_competency\api::count_competencies_in_course($course->get_id());
+            if ($ncoursecompetencies > 0) {
+                $nusercompetencies = \core_competency\api::count_proficient_competencies_in_course_for_user(
+                    $course->get_id(), $sampleid);
+                if ($ncoursecompetencies > $nusercompetencies) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             }
         }
 
