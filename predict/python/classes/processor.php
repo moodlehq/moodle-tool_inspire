d5ec1e66efbb2d8e2bbd7d968832c68d47884aab
David Monllao <david.monllao@gmail.com>
2017-03-10 01:51:53 +0100

    MDL-57791 inspire: Bump python backend version

--- predict/python/classes/processor.php
+++ predict/python/classes/processor.php
@@ -35,7 +35,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 class processor implements \tool_inspire\predictor {
 
-    const REQUIRED_PIP_PACKAGE_VERSION = '0.0.2';
+    const REQUIRED_PIP_PACKAGE_VERSION = '0.0.5';
 
     public function is_ready() {
 
